1f996c51a591a006053539c7d25d24e131839103
zzzheng212 <125420902+zzzheng212@users.noreply.github.com>
2025-01-02 17:41:25 +0800

    Add files via upload

---
+++ week14/array_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+
+    int n[5];
+
+    for(size_t i=0; i < 5;++i){
+
+        n[i]=0;
+    }
+
+    printf("%s%13s\n","Element","Value");
+
+    for(size_t i=0;i<5;++i){
+        printf("%7u%13d\n",i,n[1]);
+    }
+
+}
+
